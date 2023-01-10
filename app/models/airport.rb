f68772377f993a868c906e9013633aacb9a92073
Miguel Figuera <miguelqui725@gmail.com>
2023-01-10 13:52:24 -0400

    log 1.1, associations between models: flight, airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+    has_many :departing_flight, class_name:'Flight', foreign_key: 'flight_id'
+    has_many :arrival_flight, class_name: 'Flight', foreign_key: 'flight_id'
 end
