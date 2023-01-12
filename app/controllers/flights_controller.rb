390b87e4ca288fd4267d1a45dc78f7e6a9457f49
Miguel Figuera <miguelqui725@gmail.com>
2023-01-11 23:40:21 -0400

    log03, seeding done, search form done (working on showing the results), the flights are now ordered by dates and the dates are formated to improve readibility

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,23 @@
 class FlightsController < ApplicationController
     def index
+
+        @departures=Flight.all.map{|f|[f.departure_airport.code]}.uniq
+        @arrivals=Flight.all.map{|f|[f.arrival_airport.code]}.uniq
+        @dates=Flight.all.map{|d| [d.departure_time.strftime("%d/%m/%Y")]}
         @flights=Flight.all
+
     end
 
     def new
         @flight=Flight.new
+    end
+
+    def show
+        @departures=Flight.all.map{|f|[f.departure_airport.code,f.departure_airport.id]}
+        @arrivals=Flight.all.map{|f|[f.arrival_airport.code,f.arrival_airport.id]}
+        @dates=Flight.all.map{|d| [d.departure_time]}
+
+        @flight=Flight.find(params[:id])
     end
 
     def create 
@@ -19,6 +32,8 @@ class FlightsController < ApplicationController
 
 
     private
+
+
     def flight_params 
         params.require(:flight).permit([:id,:departure_id,:arrival_id,:duration,:departure_time,:flight_number])
     end
