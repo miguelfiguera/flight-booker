1cc5a66199355638fd3d02468fc0e4d1fb49563b
Miguel Figuera <miguelqui725@gmail.com>
2023-01-13 12:32:30 -0400

    log05, creating bookings and passengers

---
+++ db/migrate/20230113161416_updown_bookings_and_passengers.rb
@@ -0,0 +1,16 @@
+class UpdownBookingsAndPassengers < ActiveRecord::Migration[7.0]
+  def change
+    def up
+      add_column :bookings, :flight_id,:integer
+      add_column :bookings, :passenger_id, :integer
+      add_column :passengers, :name, :string
+      add_column :passengers, :booking_id, :integer
+  end
+
+  def down
+    remove_column :bookings, :flight_id
+    remove_column :bookings, :passenger_id
+    remove_column :passengers, :name
+    remove_column :passengers, :booking_id
+  end
+end
